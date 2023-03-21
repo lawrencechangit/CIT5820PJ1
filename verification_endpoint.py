0b8213b8b4256086c01c070d4198489846a6667c
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-21 23:54:09 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -10,6 +10,8 @@ app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
+    content=request.get_json(silent=True)
+    
     signature = content['sig']
     pk = content['payload']['pk']
     platform = content['payload']['platform']

ae92d55ba9a3afb0bcfd1857e9c33be7ec1e37ed
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-21 12:13:18 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -12,27 +12,23 @@ app.url_map.strict_slashes = False
 def verify():
     content = request.get_json(silent=True)
 
-    signature=content['sig']
-    pk=content['payload']['pk']
-    platform=content['payload']['platform']
-    message=content['payload']['message']
+    signature = content['sig']
+    pk = content['payload']['pk']
+    platform = content['payload']['platform']
+    message = content['payload']['message']
+    print(signature, pk, platform, message)
+    result = False
 
-    result=False
+    # Check if signature is valid
 
-    #Check if signature is valid
+    if (platform == 'Ethereum'):
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
+        eth_sig_obj = signature
+        print(eth_sig_obj)
+        if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj) == pk:
+            result = True
 
-    if(platform=='Ethereum'):
-      eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
-      eth_sig_obj = signature
-
-      if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == pk:
-        result=True 
-
-    #else if(platform=="Algorand"){
-      #if (algosdk.util.verify_bytes(message.encode('utf-8'),signature,pk)):
-          #result=True
-    #}
-
+    print(result)
     return jsonify(result)
 
 if __name__ == '__main__':

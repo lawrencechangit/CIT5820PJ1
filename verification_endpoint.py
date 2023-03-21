d8b3e710ba0c3b7279bd9e14bbefbf2c06977580
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-21 22:24:55 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -10,23 +10,33 @@ app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
+    def verify():
     signature = content['sig']
     pk = content['payload']['pk']
     platform = content['payload']['platform']
     message = content['payload']['message']
     result = False
-    
+
     if (platform == 'Ethereum'):
         eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
         eth_sig_obj = signature
+        print(eth_sig_obj)
+        print(eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj))
         if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj) == pk:
+            print("It verifies!")
             result = True
-    
-    elif (platform == 'Algorand'):
+
+    if platform == 'Algorand':
+
         payload = message
         algo_pk = pk
+
         if algosdk.util.verify_bytes(payload.encode('utf-8'), signature, algo_pk):
+            print("It verifies!")
             result = True
+
+        else:
+            print("Not verified!")
     
     return jsonify(result)
 

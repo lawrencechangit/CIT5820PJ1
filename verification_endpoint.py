14aa57109dc035f8d728b0ea89fb2eb78947648d
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-21 11:59:09 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,13 +11,6 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-    /*
-    {'sig': '0x3718eb506f445ecd1d6921532c30af84e89f2faefb17fc8117b75c4570134b4967a0ae85772a8d7e73217a32306016845625927835818d395f0f65d25716356c1c', 
- 'payload': 
-   {'message': 'Ethereum test message', 
-    'pk': '0x9d012d5a7168851Dc995cAC0dd810f201E1Ca8AF', 
-    'platform': 'Ethereum'}}
-   */
 
     signature=content['sig']
     pk=content['payload']['pk']

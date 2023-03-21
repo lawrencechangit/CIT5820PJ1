3d433e0dd12364b923a55fd8fbdd2fa9d4de1419
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-21 11:58:28 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,30 +11,37 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
+    /*
+    {'sig': '0x3718eb506f445ecd1d6921532c30af84e89f2faefb17fc8117b75c4570134b4967a0ae85772a8d7e73217a32306016845625927835818d395f0f65d25716356c1c', 
+ 'payload': 
+   {'message': 'Ethereum test message', 
+    'pk': '0x9d012d5a7168851Dc995cAC0dd810f201E1Ca8AF', 
+    'platform': 'Ethereum'}}
+   */
 
-    #Check if signature is valid
-    result = False #Should only be true if signature validates
-    
     signature=content['sig']
     pk=content['payload']['pk']
     platform=content['payload']['platform']
     message=content['payload']['message']
-    
-    #if(platform=='Ethereum'):
-        #eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
-        
-        #if(eth_account.Account.recover_message(eth_encoded_msg,signature.hex()) == pk):
-           
-         #result=True
-    
-    
-    if (platform=='Algorand'):
-        #if(algosdk.util.verify_bytes(message.encode('utf-8'),signature,pk)):
-        payload = message
 
-        #if algosdk.util.verify_bytes(payload.encode('utf-8'),signature,pk):
-        result=True
-    
+    result=False
+
+    #Check if signature is valid
+
+    if(platform=='Ethereum'){
+      eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
+      eth_sig_obj = signature
+
+
+      if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == pk:
+        result=True 
+    }
+
+    #else if(platform=="Algorand"){
+      #if (algosdk.util.verify_bytes(message.encode('utf-8'),signature,pk)):
+          #result=True
+    #}
+
     return jsonify(result)
 
 if __name__ == '__main__':

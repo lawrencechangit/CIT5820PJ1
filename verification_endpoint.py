fb5683998d74db9f34355f340f4f26947fccf98d
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-21 20:38:47 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -10,34 +10,28 @@ app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
-    content = request.get_json(silent=True)
-    
-    rawsignature=content['sig']
-    signature = hex(int(content['sig'],16))
+    signature = content['sig']
     pk = content['payload']['pk']
     platform = content['payload']['platform']
-    message = content['payload']['message']
-    print(signature, pk, platform, message)
+    message = content['payload']['message'].strip()
     result = False
 
-    # Check if signature is valid
-
-    #if (platform == 'Ethereum'):
-        #eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
-        #eth_sig_obj = signature
-        #print(eth_sig_obj)
-        #print(eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj))
-        #if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj) == pk:
-            #print("True!")
-            #result = True
-        #else:
-            #print("failed to verify!")
-
     if (platform == 'Algorand'):
+        print(" ")
         payload = message
-        algo_pk= pk
-        if algosdk.util.verify_bytes(payload.encode('utf-8'), rawsignature, algo_pk):
-            result=True
+
+        algo_pk = pk
+
+        print("Encoded message is ", payload.encode('utf-8'))
+        print("Public key is", algo_pk)
+        print("Signature is", signature)
+
+        if algosdk.util.verify_bytes(payload.encode('utf-8'), signature, algo_pk):
+            print("It verifies!")
+            result = True
+
+        else:
+            print("Not verified!")
     
     return jsonify(result)
 

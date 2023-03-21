3b521a34bd0faff564257082a57059a7e1f1758e
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-21 22:50:28 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -12,23 +12,20 @@ app.url_map.strict_slashes = False
 def verify():
     signature = content['sig']
     pk = content['payload']['pk']
+    payload=content['payload']
     platform = content['payload']['platform']
     message = content['payload']['message']
     result = False
 
     if platform == 'Ethereum':
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
         eth_sig_obj = signature
-        print(eth_sig_obj)
-        print(eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj))
         if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj) == pk:
             result = True
 
     elif platform == 'Algorand':
-
         payload = message
         algo_pk = pk
-
         if algosdk.util.verify_bytes(payload.encode('utf-8'), signature, algo_pk):
             result = True
 

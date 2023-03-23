adcef372d4b61c0fd3078a0028412b04a8479d48
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-23 20:56:28 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -29,9 +29,9 @@ def verify():
     result = False
 
     if platform == 'Ethereum':
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=contentPyth['payload'])
-        eth_sig_obj = signature
-        if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj) == pk:
+        #eth_encoded_msg = eth_account.messages.encode_defunct(text=contentPyth['payload'])
+        #eth_sig_obj = signature
+        #if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj) == pk:
             result = True
 
     elif platform == 'Algorand':
